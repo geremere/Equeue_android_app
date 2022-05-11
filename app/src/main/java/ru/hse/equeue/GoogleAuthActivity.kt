98895f255974810b2b9820fbff94165f0ead8189
rmimamov <rad.imamow@yandex.ru>
2022-05-11 23:35:28 +0300

    add loading

--- app/src/main/java/ru/hse/equeue/GoogleAuthActivity.kt
+++ app/src/main/java/ru/hse/equeue/GoogleAuthActivity.kt
@@ -1,7 +1,10 @@
 package ru.hse.equeue
 
 import android.content.Intent
+import android.opengl.Visibility
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import androidx.activity.viewModels
@@ -12,6 +15,10 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import okhttp3.Dispatcher
+import okio.Timeout
 
 import ru.hse.equeue.databinding.AuthActivityBinding
 import ru.hse.equeue.network.settings.Singletons
@@ -20,29 +27,16 @@ class GoogleAuthActivity : AppCompatActivity() {
 
     private lateinit var binding: AuthActivityBinding
 
-    override fun onStart() {
-        super.onStart()
-        Singletons.init(applicationContext)
-
-        val account: GoogleSignInAccount? = GoogleSignIn.getLastSignedInAccount(this)
-        if (account != null) {
-            val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail()
-                .requestIdToken("608288448751-cgjuabq7onf4oi1jg9d8hh6gdq4oma1k.apps.googleusercontent.com")
-                .build()
-            val mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
-            val signInIntent = mGoogleSignInClient.signInIntent
-            startActivityForResult(signInIntent, 200)
-        }
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        Singletons.init(applicationContext)
+        val account = GoogleSignIn.getLastSignedInAccount(this)
         binding = AuthActivityBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
-        findViewById<Button>(R.id.google_signIn).setOnClickListener {
+
+        if (account != null) {
+            binding.googleSignIn.visibility = View.GONE
+            binding.loading.loading.visibility = View.VISIBLE
             val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
                 .requestIdToken("608288448751-cgjuabq7onf4oi1jg9d8hh6gdq4oma1k.apps.googleusercontent.com")
@@ -51,7 +45,25 @@ class GoogleAuthActivity : AppCompatActivity() {
             val signInIntent = mGoogleSignInClient.signInIntent
             startActivityForResult(signInIntent, 200)
         }
+        setContentView(binding.root)
+        initBinding()
     }
+
+    private fun initBinding() {
+        binding.googleSignIn.setOnClickListener {
+            val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .requestIdToken("608288448751-cgjuabq7onf4oi1jg9d8hh6gdq4oma1k.apps.googleusercontent.com")
+                .build()
+            val mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+            val signInIntent = mGoogleSignInClient.signInIntent
+            startActivityForResult(signInIntent, 200)
+            binding.googleSignIn.visibility = View.GONE
+            binding.loading.loading.visibility = View.VISIBLE
+        }
+
+    }
+
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
@@ -68,3 +80,5 @@ class GoogleAuthActivity : AppCompatActivity() {
         }
     }
 }
+
+
